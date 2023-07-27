339be76afffc9063f4202051ec3352938779e745
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-27 15:39:06 +0300

    Add project files.

---
+++ Demo2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // program to print all the odd numbers between 1 and 100
+            int I = 0;
+
+            while (I <=100)
+            {
+                if(I % 2 == 1)
+                {
+                    Console.WriteLine(I);
+                }
+                I++;
+            }
+        }
+    }
+}
