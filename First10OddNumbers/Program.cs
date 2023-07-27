2fcbc04c5445a6e059b07e9fd5562dd81463ace8
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-27 15:52:33 +0300

    program to print the first 10 odd numbers

---
+++ First10OddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace First10OddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // program to print the first 10 odd numbers
+            int I = 0;
+            int counter = 1;
+            Console.WriteLine("Top 10 Odd Numbers");
+
+            while (I <= 100 && counter <= 10)
+            {
+                if (I % 2 == 1)
+                {
+                    Console.WriteLine($"{counter}  -  {I}");
+                    counter++;
+                }
+                I++;
+            }
+        }
+    }
+}
